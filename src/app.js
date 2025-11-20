3a338de446b84fc08a95b54321f788db8cbc174d
f4-bit <fabiancquinterop@gmail.com>
2025-11-19 22:23:37 -0500

    Cors updated

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Cors configuration
 app.use(cors({
-  origin: "http://localhost:3000",           // Local Frontend
+  origin: "https://bloodflow-mu.vercel.app",
   methods: "GET,POST,PUT,DELETE,OPTIONS",
   allowedHeaders: "Content-Type,Authorization",
   credentials: true
